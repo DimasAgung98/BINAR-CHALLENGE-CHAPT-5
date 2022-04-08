5016a237c199d81302d96af50025c3b378ac2a4c
dimas agung <dimasraden0798@gmail.com>
2022-04-08 22:19:40 +0800

    Menambahkan Middleware, controller, dan routes

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ const express = require('express');
 const app = express();
 //localhost port
 const PORT = 8000;
-//declare ejs framework
+//declare ejs view engine
 app.set('view engine', 'ejs');
-
-//Running express server using port 8000
-app.listen(PORT, () => {
-  console.log(`Server is Runnning On Port ${PORT}`);
-})
 
 //MIDDLEWARE
 app.use('/public',express.static('public'));
@@ -28,5 +23,8 @@ app.get('/', indexController.index);
 app.get('/game', gameController.index);
 app.get('/login', loginController.index);
 
-
+//Running express server using port 8000
+app.listen(PORT, () => {
+  console.log(`Server is Runnning On Port ${PORT}`);
+})
 
